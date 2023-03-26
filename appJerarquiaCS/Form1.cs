eebef658a3a022bf71af690e69fe5b0c427902ba
Angel Zurita <af3392976@gmail.com>
2023-03-25 18:41:02 -0600

    .

--- appJerarquiaCS/Form1.cs
+++ appJerarquiaCS/Form1.cs
@@ -8,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace appJerarquiaCS
 {
@@ -22,8 +24,9 @@ namespace appJerarquiaCS
             OpenFileDialog ofd = new OpenFileDialog();
             if (ofd.ShowDialog() != DialogResult.OK) { return; }
 
-            StreamReader sr = new StreamReader(ofd.FileName);
-            LlenarArbol(sr);
+            //StreamReader sr = new StreamReader(ofd.FileName);
+            //LlenarArbol(sr);
+            filltree(ofd.FileName);
         }
         private void LlenarArbol(StreamReader sr)
         {
@@ -83,5 +86,107 @@ namespace appJerarquiaCS
 
             }
         }
+
+        private void filltree(string filename)
+        {
+            // Crear un diccionario para almacenar los códigos postales y las colonias correspondientes
+            Dictionary<string, List<string>> cpColonias = new Dictionary<string, List<string>>();
+
+            // Leer el archivo de texto que contiene los datos de los códigos postales
+            string[] lineas = File.ReadAllLines(filename);
+
+            // Recorrer cada línea del archivo de texto
+            foreach (string linea in lineas)
+            {
+                // Separar los campos de la línea por comas
+                string[] campos = linea.Split('|');
+
+                // Obtener el estado, la ciudad, el código postal y la colonia correspondientes
+                string estado = campos[4];
+                string ciudad = campos[5];
+                string cp = campos[0];
+                string colonia = campos[1];
+
+                // Verificar si el código postal ya está en el diccionario
+                if (!cpColonias.ContainsKey(cp))
+                {
+                    // Si el código postal no está en el diccionario, agregarlo con una lista vacía de colonias
+                    cpColonias.Add(cp, new List<string>());
+                }
+
+                // Agregar la colonia correspondiente a la lista de colonias del código postal
+                cpColonias[cp].Add(colonia);
+
+                // Verificar si el estado ya está en el árbol
+                TreeNode estadoNode = null;
+                foreach (TreeNode node in treeView1.Nodes)
+                {
+                    if (node.Text == estado)
+                    {
+                        estadoNode = node;
+                        break;
+                    }
+                }
+                if (estadoNode == null)
+                {
+                    // Si el estado no está en el árbol, agregarlo como nodo principal
+                    estadoNode = new TreeNode(estado);
+                    treeView1.Nodes.Add(estadoNode);
+                }
+
+                // Verificar si la ciudad ya está en el árbol
+                TreeNode ciudadNode = null;
+                foreach (TreeNode node in estadoNode.Nodes)
+                {
+                    if (node.Text == ciudad)
+                    {
+                        ciudadNode = node;
+                        break;
+                    }
+                }
+                if (ciudadNode == null)
+                {
+                    // Si la ciudad no está en el árbol, agregarla como nodo secundario del estado
+                    ciudadNode = new TreeNode(ciudad);
+                    estadoNode.Nodes.Add(ciudadNode);
+                }
+
+                // Verificar si el código postal ya está en el árbol
+                TreeNode cpNode = null;
+                foreach (TreeNode node in ciudadNode.Nodes)
+                {
+                    if (node.Text == cp)
+                    {
+                        cpNode = node;
+                        break;
+                    }
+                }
+                if (cpNode == null)
+                {
+                    // Si el código postal no está en el árbol, agregarlo como nodo secundario de la ciudad
+                    cpNode = new TreeNode(cp);
+                    ciudadNode.Nodes.Add(cpNode);
+                }
+
+                // Verificar si la colonia ya está en el árbol
+                TreeNode coloniaNode = null;
+                foreach (TreeNode node in cpNode.Nodes)
+                {
+                    if (node.Text == colonia)
+                    {
+                        coloniaNode = node;
+                        break;
+                    }
+                }
+                if (coloniaNode == null)
+                {
+                    // Si la colonia no está en el árbol, agregarla como nodo secundario del código postal
+                    coloniaNode = new TreeNode(colonia);
+                    cpNode.Nodes.Add(coloniaNode);
+                }
+            }
+        }
+        
+
     }
 }
